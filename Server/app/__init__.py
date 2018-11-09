5aaa0987aceac9b46104b51c819961445963bd16
Goming <miraedbswo@gmail.com>
2018-11-09 20:25:33 +0900

    create_app function

---
+++ Server/app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flasgger import Swagger
+import mongoengine
+
+from app.api import router
+from config import IMAGE_FOLDER_PATH
+
+
+def create_app(*config_obj):
+    app_ = Flask(
+        __name__,
+        static_folder=IMAGE_FOLDER_PATH,
+    )
+
+    for obj in config_obj:
+        app_.config.from_object(obj)
+
+    mongoengine.connect(**app_.config["MONGODB_SETTINGS"])
+
+    router(app_)
+
+    JWTManager(app_)
+    # Swagger(template=app_.config['SWAGGER_TEMPLATE']).init_app(app_)
+
+    return app_
