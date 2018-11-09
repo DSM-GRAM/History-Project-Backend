8c45764f1f93f231267e805261ea01bd62279286
Goming <miraedbswo@gmail.com>
2018-11-09 20:26:00 +0900

    image url route

---
+++ Server/app/api/image.py
@@ -0,0 +1,31 @@
+from flask import send_file, Blueprint, make_response
+from flask_restful import Api, Resource
+
+from config import IMAGE_FOLDER_PATH
+
+image_blueprint = Blueprint(__name__, __name__)
+api = Api(image_blueprint)
+
+
+@api.resource('/image/<image_name>')
+class ImageView(Resource):
+    def get(self, image_name):
+        try:
+            image = make_response(send_file(f'{IMAGE_FOLDER_PATH}\{image_name}.jpg',
+                                            attachment_filename=f'{image_name}.jpg'))
+            if image is None:
+                raise TypeError
+
+            return image
+
+        except TypeError as e:
+            return str(e), 204
+
+
+@api.resource('/main/<location>')
+class LocationView(Resource):
+    def get(self, location):
+        if location == 'Vladivostok':
+            pass
+        else:
+            pass
