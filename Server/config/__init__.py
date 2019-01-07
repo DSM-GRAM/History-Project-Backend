2b1fb8f9f164697527a03c6e2be50948d652abc8
Goming <miraedbswo@gmail.com>
2019-01-07 21:53:13 +0900

    FIX: ADD Flasgger Template

--- Server/config/__init__.py
+++ Server/config/__init__.py
@@ -24,3 +24,23 @@ class Config:
         'username': os.getenv('MONGO_ID', None),
         'password': os.getenv('MONGO_PW', None)
     }
+
+    SWAGGER = {
+        'title': SERVICE_NAME,
+        'specs_route': os.getenv('SWAGGER_URI', '/docs'),
+        'uiversion': 3,
+
+        'info': {
+            'title': SERVICE_NAME + ' API',
+            'version': '1.0',
+            'description': ''
+        },
+        'basePath': '/',
+    }
+
+    SWAGGER_TEMPLATE = {
+        'schemes': [
+            'http'
+        ]
+    }
+
