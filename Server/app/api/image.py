06c47dd956e2203c20413ad9b605d6688da5a0ce
Goming <miraedbswo@gmail.com>
2019-01-07 21:51:47 +0900

    FIX: '\' to '\\' in address string

--- Server/app/api/image.py
+++ Server/app/api/image.py
@@ -1,7 +1,9 @@
 from flask import send_file, Blueprint, make_response, abort
+from flasgger import swag_from
 from flask_restful import Api
 
 from app.api import BaseResource
+from app.docs.image import IMAGE_GET
 from config import IMAGE_FOLDER_PATH
 
 image_blueprint = Blueprint(__name__, __name__)
@@ -10,15 +12,12 @@ api = Api(image_blueprint)
 
 @api.resource('/image/<image_name>')
 class ImageView(BaseResource):
+    @swag_from(IMAGE_GET)
     def get(self, image_name):
         try:
-            image = make_response(send_file(f'{IMAGE_FOLDER_PATH}\{image_name}.jpg',
+            image = make_response(send_file(f'{IMAGE_FOLDER_PATH}\\{image_name}.jpg',
                                             attachment_filename=f'{image_name}.jpg'))
             return image
-
         except FileNotFoundError:
-            return f'{image_name}.jpg is not found', 401
-
-
-
+            abort(404)
 
