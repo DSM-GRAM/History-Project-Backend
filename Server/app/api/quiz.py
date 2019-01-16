1706ac8c4c701d7ac7053b1cd13175adf23d719f
miraedbswo <miraedbswo@gmail.com>
2019-01-16 14:21:46 +0900

    ADD Docs

--- Server/app/api/quiz.py
+++ Server/app/api/quiz.py
@@ -1,48 +1,36 @@
-from random import randint
-
-from flask import Blueprint, request, abort
+from flask import Blueprint
 from flask_restful import Api
+from flasgger import swag_from
 
 from app.api import BaseResource
-from app.models.quiz import QuizModel
+from app.models.quiz import OXQuizModel, MultipleQuizModel
+from app.docs.quiz import MULTIPLE_QUIZ_GET, OX_QUIZ_GET
 
 quiz_blueprint = Blueprint(__name__, __name__)
 api = Api(quiz_blueprint)
 
 
-@api.resource('/quiz')
-class QuizView(BaseResource):
-    def get(self):
-        quiz_list = QuizModel.objects().all()
-        list_len = len(quiz_list)
-        if not list_len:
-            abort(204)
+@api.resource('/quiz/ox/<area>')
+class OXQuizView(BaseResource):
+    @swag_from(OX_QUIZ_GET)
+    def get(self, area):
+        quiz = OXQuizModel.objects(area=area).first()
 
-        quiz = quiz_list[randint(0, len(quiz_list) - 1)]
+        return self.unicode_safe_json_dumps({
+            "question": quiz.question,
+            "OXAnswer": quiz.answer
+        }, 200)
 
-        if "O" in quiz.answer or "X" in quiz.answer:
-            answer = quiz.answer[0]
 
-            return self.unicode_safe_json_dumps({
-                "question": quiz.question,
-                "OXanswer": answer
-            }, 200)
+@api.resource('/quiz/multiple/<area>')
+class MultipleQuizView(BaseResource):
+    @swag_from(MULTIPLE_QUIZ_GET)
+    def get(self, area):
+        quiz = MultipleQuizModel.objects(area=area).first()
 
-        else:
-            return self.unicode_safe_json_dumps({
-                "question": quiz.question,
-                "answer_word": quiz.answer,
-                "NumberOfAnswer": len(quiz.answer),
-                "WordList": quiz.WordList
-            }, 200)
-
-    # Test
-
-    def post(self):
-        QuizModel(
-            question=request.json['question'],
-            answer=request.json['answer'],
-            WordList=request.json['WordList']
-        ).save()
-
-        return '', 201
+        return self.unicode_safe_json_dumps({
+            "question": quiz.question,
+            "answer_word": quiz.answer,
+            "NumberOfAnswer": len(quiz.answer),
+            "WordList": quiz.WordList
+        }, 200)
---
+++ Server/app/docs/quiz.py
@@ -0,0 +1,37 @@
+from app.docs import parameter
+
+OX_QUIZ_GET = {
+    'tags': ['Quiz'],
+    'description': '유적지에 해당하는 ox 퀴즈 정보륿 불러옵니다.',
+    'parameters': [
+        parameter('area', '유적지 이름', 'url')
+    ],
+    'responses': {
+        '200': {
+            'description': 'OX Quiz 조회 성공',
+            'examples': {
+                'question': '구 개척리에서 강제로 후에...',
+                'OXAnswer': 'X'
+            }
+        }
+    }
+}
+
+MULTIPLE_QUIZ_GET = {
+    'tags': ['Quiz'],
+    'description': '유적지에 해당하는 multiple choice question 퀴즈 정보를 불러옵니다.',
+    'parameters': [
+        parameter('area', '유적지 이름', 'url')
+    ],
+    'responses': {
+        '200': {
+            'description': '유적지 좌표 정보 조회 성공',
+            'examples': {
+                "question": '신한촌 내 한인들의 자체 단체인 ( ) 은/는 일제와 러시아의 탄압을 피해...',
+                "answer_word": '권업회',
+                "NumberOfAnswer": '1',
+                "WordList": '신민회, 의열단, 권업회, 광복회'
+            }
+        },
+    }
+}
